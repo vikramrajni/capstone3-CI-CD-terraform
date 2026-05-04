0559beee38d00c98f69817b37a789b32953e36d6
Vikram <vikramrajni@yahoo.com>
2026-05-04 15:46:41 -0400

    added S3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,15 @@
+terraform {
+  required_version = ">= 0.12"
+  backend "s3" {
+    bucket = "myapp-vik-tf-s3-bucket"
+    key = "myapp/state.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
-
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
